24ff73259629ec05caa9d311082834f2bb8997ff
Ildar-Ysupov <aismen56@mail.ru>
2022-08-12 09:56:48 +0500

    задача 1

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат.
+// Нпример: 4 -> 16, -3 -> 9, -7 -> 49.
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+//int result = Convert.ToInt32(Math.Pow(number,2)); второй способ
+
+Console.WriteLine("Квадрат от числа " + number + " = " + square);
+
+// Склеить строки - конкатенация
