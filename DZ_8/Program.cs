6798909f2e78b41faa83cec33f4c917f7161ead9
Ildar-Ysupov <aismen56@mail.ru>
2022-08-12 14:51:48 +0500

    задача на дом 8

---
+++ DZ_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу,которая на вход принимает число (N), а на выходе
+//показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;   
+
+while (evenNumber <= number )
+{
+    Console.Write(evenNumber + " ");
+    evenNumber+=2;
+}
