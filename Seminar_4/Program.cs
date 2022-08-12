207b160a44ebcf4d588fee10cb63c294eb15a321
Ildar-Ysupov <aismen56@mail.ru>
2022-08-12 11:12:59 +0500

    задача 4

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), А НА выходе показывает
+//  все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+int number = Convert.ToInt32(Console.ReadLine());   // n - number
+int negativeNumber = number*(-1);    // -n = n*(-1)
+
+while (negativeNumber <= number )
+{
+    Console.Write(negativeNumber + " ");
+    negativeNumber++;
+}
